8815ef838e180bf5451c37faa3697e36b6c558ac
velkonini <avelkov07@gmail.com>
2022-02-18 19:58:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# #Задача 1
+# number = int(input('Введите число: '))
+# third_number = number % 10
+# sec_number = int(number / 10) % 10
+# first_number = int(int(number / 10) / 10)
+#
+# sum = first_number + sec_number + third_number
+# mult = first_number * sec_number * third_number
+#
+# print(first_number, sec_number, third_number)
+# print('Сумма цифр трехзначного числа: ' + str(sum))
+# print('Результат умножения трехзначного числа: ' + str(mult))
+#
+
+
+
